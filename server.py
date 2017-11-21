5c7c244f76fba8b3c31c3368ce7d84e9e77fe2cc
Mworrall Dev Comp <mworrall101@gmail.com>
2017-11-20 23:06:56 -0800

    Added some code to the server as well as regressor.p, X_test.p and y_test.p

--- server.py
+++ server.py
@@ -2,11 +2,18 @@ import sys
 
 from flask import Flask, render_template, request, redirect, Response
 import random, json
+import pickle
+
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+
 
 app = Flask(__name__)
 
 #
 #   FUNCTIONS TO TRAIN MODEL AND GET FINAL VALUES ETC
+
+
 #
 def getFinalValue():
     return 65;
@@ -46,9 +53,15 @@ def worker():
 
 if __name__ == '__main__':
     #
-    #   TRAIN THE MODEL RIGHT HERE
-    #
+    #   Here I loaded the model and test data from binary files
+    #   If we can load the data from the web interface into the right format, we should be able to use
+    #   the predict function to get a price
+	regressor = pickle.load(open("regressor.p", "rb"))
+	X_test = pickle.load(open("X_test.p", "rb"))
+	y_test = pickle.load(open("y_test.p", "rb"))
 
+	print("Score: ", regressor.score(X_test, y_test))
+	print("Sample Predictions: " + str(regressor.predict(X_test[:5])))
 
     # run!
-    app.run()
+app.run()
